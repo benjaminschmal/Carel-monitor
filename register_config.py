9daa1e736aa7dd35639689196b4cd97c3abbdf3d
Benjamin Schmal <58697402+benjaminschmal@users.noreply.github.com>
2026-08-27 09:39:11 +0200

    feat: expand verified Dimplex register mapping

--- register_config.py
+++ register_config.py
@@ -1,21 +1,39 @@
 REGISTER_MAP = {
 
-    2: {
-        "name": "Vorlauf",
+    1: {
+        "name": "Außentemperatur",
         "unit": "°C",
         "favorite": True,
     },
 
-    3: {
+    2: {
         "name": "Rücklauf",
         "unit": "°C",
         "favorite": True,
     },
 
-    6: {
-        "name": "Außentemperatur",
+    3: {
+        "name": "Warmwasser",
         "unit": "°C",
         "favorite": True,
+    },
+
+    5: {
+        "name": "Vorlauf",
+        "unit": "°C",
+        "favorite": True,
+    },
+
+    6: {
+        "name": "Wärmequelleneintritt",
+        "unit": "°C",
+        "favorite": False,
+    },
+
+    7: {
+        "name": "Wärmequellenaustritt",
+        "unit": "°C",
+        "favorite": False,
     },
 
 }
